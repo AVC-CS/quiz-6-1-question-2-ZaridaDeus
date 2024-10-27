99084e0c5ec33b5dcee8ed93099fe7e5cbcdbe86
ZaridaDeus <holau909@gmail.com>
2024-10-26 17:45:43 -0700

    1st Run

--- main.hpp
+++ main.hpp
@@ -1,3 +1,37 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
 /***************************************************
  * Code your program here
  ***************************************************/
+int writeFile(string);
+int readFile(string);
+
+int writeFile(string file) {
+    ofstream writer;
+    writer.open(file);
+    int N, ID, Wage;
+    string name, Department;
+    cin >> N;
+    writer << N << "\n";
+    for (int i = 0; i < N; i++) {
+        cin >> ID >> name >> Department >> Wage;
+        writer << ID << "\t" << name << "\t" << Department << "\t" << Wage << "\n";
+    }
+    writer.close();
+    return N;
+}
+
+int readFile(string file) {
+    ifstream reader;
+    reader.open(file);
+    int N, ID, Wage;
+    string name, Department;
+    reader >> N;
+    for (int i = 0; i < N; i++) {
+        reader >> ID >> name >> Department >> Wage;
+        cout << ID << " " << name << " " << Department << " " << Wage << "\n";
+    }
+    reader.close();
+    return N;
+}
